20c17557a6cc89f7f9977a8267d213c98e8379ed
Andrey Novikov <anovikovrf@gmail.com>
2022-08-04 18:39:05 +0300

    added locales

---
+++ src/locales/en.js
@@ -0,0 +1,13 @@
+// перевести на англ
+export default {
+  translation: {
+    header: 'RSS Aggregator',
+    paragraph: "Start reading RSS today! It's easy, it's beautiful.",
+    label: 'RSS Link',
+    example: 'Example: https://ru.hexlet.io/lessons.rss',
+    feedback: 'RSS has been successfully loaded', // а если нет? надо подумать над организацией ошибок
+    // RSS уже существует   Ресурс не содержит валидный RSS   Ссылка должна быть валидным URL
+    btn: 'Add',
+    lngBtn: 'ru',
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    header: 'RSS агрегатор',
+    paragraph: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    label: 'Ссылка RSS',
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    feedback: 'RSS успешно загружен', // а если нет? надо подумать над организацией ошибок
+    // RSS уже существует   Ресурс не содержит валидный RSS   Ссылка должна быть валидным URL
+    btn: 'Добавить',
+    lngBtn: 'en',
+  },
+};
